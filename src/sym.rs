42c9a528190e45a3d58786e48a6d0f583a6770ac
Andrew Chi <chifamicom@outlook.com>
2026-06-20 16:51:01 -0400

    remove unused symbols

--- src/sym.rs
+++ src/sym.rs
@@ -1,17 +1,7 @@
 pub mod fun {
     emacs::use_functions! {
         BUFFER_SUBSTRING_NO_PROPERTIES => "buffer-substring-no-properties"
-        GOTO_CHAR => "goto-char"
-        EOBP => "eobp"
-        MARKER_POSITION => "marker-position"
-        MIN => "min"
-        POINT => "point"
         POINT_MAX => "point-max"
         POINT_MIN => "point-min"
-        POINT_MIN_MARKER => "point-min-marker"
-        PLUS => "+"
-        MINUS => "-"
-        SET_MARKER => "set-marker"
-        GEQ => ">="
     }
 }
