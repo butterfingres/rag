27abfdec08d697e82ee409835d8693a6bd33f24c
Andrew Chi <chifamicom@outlook.com>
2026-06-24 13:43:46 -0400

    remove tests directory

--- src/xml/rss_2_0/mod.rs
+++ src/xml/rss_2_0/mod.rs
@@ -1,6 +1,3 @@
-#[cfg(test)]
-mod tests;
-
 use {
     crate::{
         borrow::Cow,
@@ -448,5 +445,74 @@ where
 
             (step, _) => Ok(step),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use {
+        super::*,
+        crate::{
+            alloc, tz,
+            xml::tests::{TestParserError, test_parser},
+        },
+        allocator_api2::{alloc::Global, vec},
+        jiff::{civil::datetime, tz::TimeZone},
+    };
+
+    #[test]
+    fn test_rss_parser_all() -> Result<(), TestParserError<'static>> {
+        test_parser::<_, Step, _>(
+            include_str!("./all.xml"),
+            Channel {
+                title: Some(Cow::Borrowed(b"example feed")),
+                link: Some(Cow::Borrowed(b"https://example.com/rss")),
+                modify_date: Some(Replaceable {
+                    data: datetime(2023, 07, 21, 09, 04, 00, 00)
+                        .to_zoned(tz::EDT)?
+                        .timestamp()
+                        .into(),
+                    replaceable: false,
+                }),
+                skip_hours: SkipHours::new([0b0111]),
+                skip_days: SkipDays::new([0b0111]),
+            },
+            [Entry {
+                title: Some(Cow::Borrowed(b"entry 1")),
+                link: Some(Cow::Borrowed(b"https://example.com/entry_1")),
+                description: Some(Cow::Borrowed(b"the first entry")),
+                pub_date: datetime(2003, 06, 20, 09, 00, 00, 00)
+                    .to_zoned(TimeZone::UTC)?
+                    .timestamp()
+                    .into(),
+                enclosures: vec![in &Global; Enclosure {
+                    tag: BytesStart::from_content(r#"enclosure url="https://example.com/entry_1.mp3""#, "enclosure".len()),
+                    enclosure: r#"enclosure url=""#.len()..r#"enclosure url="https://example.com/entry_1.mp3"#.len(),
+                }],
+            }],
+            &Global,
+        )
+    }
+
+    #[test]
+    fn test_rss_parser_alt() -> Result<(), TestParserError<'static>> {
+        test_parser::<_, Step, _>(
+            include_str!("./alt.xml"),
+            Channel {
+                title: Some(Cow::Borrowed(b"example feed")),
+                link: Some(Cow::Borrowed(b"https://example.com/rss")),
+                modify_date: Some(Replaceable {
+                    data: datetime(2023, 07, 21, 09, 04, 00, 00)
+                        .to_zoned(tz::EDT)?
+                        .timestamp()
+                        .into(),
+                    replaceable: false,
+                }),
+                skip_hours: SkipHours::default(),
+                skip_days: SkipDays::new([0b0111_1111]),
+            },
+            [],
+            &alloc::Dummy,
+        )
     }
 }
