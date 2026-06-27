b339390cb904b1cbe8b4014b2fdabd04992141ba
Andrew Chi <chifamicom@outlook.com>
2026-06-27 16:41:19 -0400

    start updates

--- lisp/rag.el
+++ lisp/rag.el
@@ -11,9 +11,26 @@
 
 ;;; Code:
 
+(eval-when-compile (require 'cl-macs))
+(require 'url-queue)
+
 (defgroup rag '()
   "Rust news AGgragator."
   :group 'news)
+
+(cl-defstruct rag-source
+  "Feed source."
+  url
+  tags)
+
+(defun rag-source-update (source)
+  "Update source SOURCE."
+  (letrec ((buffer (url-queue-retrieve
+                    (rag-source-url source)
+                    (lambda (status)
+                      (unwind-protect
+                          nil
+                        (kill-buffer buffer))))))))
 
 (define-derived-mode rag-mode special-mode "RAG"
   "Rust news AGgragator."
