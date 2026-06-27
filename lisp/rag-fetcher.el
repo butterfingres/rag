ecbc4270cbf6a58709e3ad40ee3ee46819e2202c
Andrew Chi <chifamicom@outlook.com>
2026-06-27 16:36:04 -0400

    remove fetcher backends
    i will just default to url-retrieve

--- lisp/rag-fetcher.el
+++ lisp/rag-fetcher.el
@@ -13,18 +13,6 @@
 
 (eval-when-compile (require 'cl-macs))
 
-(cl-defstruct rag-fetcher-config
-  "Fetcher generic fetcher configuration."
-  timeout)
-
-(cl-defgeneric rag-fetcher-fetch-url (fetcher-config backend-config url callback)
-  "Fetch URL.
-
-FETCHER-CONFIG is the `rag-fetcher-config' configuration for generic
-options.  BACKEND-CONFIG is the backend configuration options that also
-signals which backend to use.  Call CALLBACK in the buffer when
-completed.")
-
 (provide 'rag-fetcher)
 
 ;;; rag-fetcher.el ends here
