aaec7a74b6feef01d89345156d021457699af4d7
Andrew Chi <chifamicom@outlook.com>
2026-06-28 13:24:16 -0400

    add frontend buffer

--- lisp/rag.el
+++ lisp/rag.el
@@ -24,7 +24,33 @@
 
 (define-derived-mode rag-mode special-mode "RAG"
   "Rust news AGgragator."
-  :interactive nil)
+  :interactive nil
+  (let ((db (rag-db-get)))
+    (dolist (entry (sqlite-select db "SELECT title, pub_date FROM entry
+ORDER BY pub_date DESC"))
+      (let ((title (car entry))
+            (pub-date (cadr entry))
+            (inhibit-read-only t))
+        (insert (format-time-string "%Y-%m-%d" pub-date)
+                " "
+                title)
+        (newline)))))
+
+(defcustom rag-buffer-name "*rag*"
+  "The buffer name to use."
+  :group 'rag
+  :type 'string)
+
+(defun rag-buffer-get ()
+  (or (get-buffer rag-buffer-name)
+      (let ((buffer (get-buffer-create rag-buffer-name)))
+        (with-current-buffer buffer
+          (rag-mode))
+        buffer)))
+
+(defun rag-buffer-goto ()
+  (interactive)
+  (pop-to-buffer (rag-buffer-get)))
 
 (provide 'rag)
 
