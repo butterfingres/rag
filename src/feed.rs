0e740221a74d0f8a5a4cfda7bf9e75a924575d88
Andrew Chi <chifamicom@outlook.com>
2026-07-04 18:19:36 -0400

    add tests for fetch_p

--- src/feed.rs
+++ src/feed.rs
@@ -18,6 +18,35 @@ impl Display for OverflowError {
 }
 impl Error for OverflowError {}
 
+fn fetch_p_inner<T>(
+    ttl: Option<T>,
+    frequency: Option<i32>,
+    last_update: Option<i64>,
+    now: i64,
+) -> Result<bool, emacs::Error>
+where
+    T: AsRef<str>,
+{
+    if let Some(ttl) = ttl
+        && let Some(last_update) = last_update
+    {
+        let ttl = Span::from_str(ttl.as_ref())?;
+        let last_update = Timestamp::from_second(last_update)?;
+        let now = Timestamp::from_second(now)?;
+
+        let mut duration = ttl.to_duration(&last_update.to_zoned(TimeZone::UTC))?;
+        if let Some(frequency) = frequency {
+            duration = duration
+                .checked_div(frequency)
+                .ok_or(OverflowError(duration, frequency))?;
+        }
+
+        Ok(now >= last_update.checked_add(duration)?)
+    } else {
+        Ok(true)
+    }
+}
+
 /// Check if a feed should be fetched.
 ///
 /// Returns non-nil when NOW is not in the cache period. TTL is the
@@ -31,22 +60,56 @@ pub fn fetch_p(
     last_update: Option<i64>,
     now: i64,
 ) -> Result<bool, emacs::Error> {
-    if let Some(ttl) = ttl
-        && let Some(last_update) = last_update
-    {
-        let ttl = Span::from_str(&ttl)?;
-        let last_update = Timestamp::from_second(last_update)?;
-        let now = Timestamp::from_second(now)?;
+    fetch_p_inner(ttl, frequency, last_update, now)
+}
 
-        let mut duration = ttl.to_duration(&last_update.to_zoned(TimeZone::UTC))?;
-        if let Some(frequency) = frequency {
-            duration = duration
-                .checked_div(frequency)
-                .ok_or(OverflowError(duration, frequency))?;
-        }
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-        Ok(now > last_update.checked_add(duration)?)
-    } else {
-        Ok(true)
+    #[test]
+    fn test_fetch_p_empty() -> Result<(), emacs::Error> {
+        assert_eq!(
+            fetch_p_inner(None::<&str>, None, None, 0)?,
+            true,
+            "empty feeds should always be fetched"
+        );
+        Ok(())
+    }
+
+    #[test]
+    fn test_fetch_p_below_cache() -> Result<(), emacs::Error> {
+        assert_eq!(
+            fetch_p_inner(Some("PT10M"), None, Some(0), 0)?,
+            false,
+            "0 < 0 + 10"
+        );
+        Ok(())
+    }
+
+    #[test]
+    fn test_fetch_p_default() -> Result<(), emacs::Error> {
+        assert_eq!(fetch_p_inner(Some("PT1S"), None, None, 1)?, true, "1 > 0");
+        Ok(())
+    }
+
+    #[test]
+    fn test_fetch_p_last_update() -> Result<(), emacs::Error> {
+        assert_eq!(
+            fetch_p_inner(Some("PT1S"), None, Some(1), 2)?,
+            true,
+            "2 >= 1 + 1"
+        );
+        Ok(())
+    }
+
+    #[test]
+    fn test_fetch_p_frequency() -> Result<(), emacs::Error> {
+        assert_eq!(
+            fetch_p_inner(Some("PT1M"), Some(2), Some(0), 30)?,
+            true,
+            "60 / 2 >= 30"
+        );
+        Ok(())
     }
 }
