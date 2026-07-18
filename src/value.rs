bf8e2af6b0cf97ed679f39e63d11a699659fd478
Andrew Chi <chifamicom@outlook.com>
2026-07-18 10:00:49 -0400

    add symbol value

--- src/value.rs
+++ src/value.rs
@@ -11,6 +11,7 @@ where
     Cons(Box<Value<'a, A>, A>, Box<Value<'a, A>, A>),
     Char(char),
     String(&'a str),
+    Symbol(&'a str),
 }
 impl<A> Display for Value<'_, A>
 where
@@ -32,7 +33,7 @@ where
             Self::Char(' ') => f.write_str("?\\s"),
             Self::Char('\u{7f}') => f.write_str("?\\d"),
             Self::Char(
-                ch @ ('\\' | '\"' | '(' | ')' | '[' | ']' | ';' | '|' | '\'' | '`' | '#' | '.'
+                ch @ ('(' | ')' | '[' | ']' | '\\' | ';' | '\"' | '|' | '\'' | '`' | '#' | '.'
                 | ','),
             ) => {
                 write!(f, "?\\{ch}")
@@ -58,6 +59,18 @@ where
                 f.write_char('\"')?;
                 Ok(())
             }
+            Self::Symbol("") => f.write_str("##"),
+            Self::Symbol(symbol) => {
+                for ch in symbol.chars() {
+                    match ch {
+                        ch @ (' ' | '(' | ')' | '[' | ']' | '\\' | ';' | '\"' | '|' | '\''
+                        | '`' | '#' | '.' | ',') => write!(f, "\\{ch}")?,
+                        _ => f.write_char(ch)?,
+                    }
+                }
+
+                Ok(())
+            }
         }
     }
 }
@@ -67,7 +80,7 @@ mod tests {
     use {super::*, crate::alloc::Dummy};
 
     macro_rules! test_value {
-        ($ident:ident, $input:expr, $output:literal) => {
+        ($input:expr, $output:literal, $ident:ident $(,)?) => {
             #[test]
             fn $ident() -> ::std::result::Result<(), ::std::fmt::Error> {
                 let mut buf = ::arrayvec::ArrayString::<{ $output.len() }>::new();
@@ -78,155 +91,233 @@ mod tests {
         };
     }
 
-    test_value!(value_display_nil, Value::<Dummy>::Nil, "nil");
+    test_value!(Value::<Dummy>::Nil, "nil", value_display_nil);
 
     test_value!(
-        value_display_char_control_g,
         Value::<Dummy>::Char('\u{7}'),
-        "?\\a"
+        "?\\a",
+        value_display_char_control_g,
     );
     test_value!(
-        value_display_char_backspace,
         Value::<Dummy>::Char('\u{8}'),
-        "?\\b"
+        "?\\b",
+        value_display_char_backspace,
     );
-    test_value!(value_display_char_tab, Value::<Dummy>::Char('\t'), "?\\t");
+    test_value!(Value::<Dummy>::Char('\t'), "?\\t", value_display_char_tab);
     test_value!(
-        value_display_char_newline,
         Value::<Dummy>::Char('\n'),
-        "?\\n"
+        "?\\n",
+        value_display_char_newline,
     );
     test_value!(
-        value_display_char_vertical_tab,
         Value::<Dummy>::Char('\u{b}'),
-        "?\\v"
+        "?\\v",
+        value_display_char_vertical_tab,
     );
     test_value!(
-        value_display_char_form_feed,
         Value::<Dummy>::Char('\u{c}'),
-        "?\\f"
+        "?\\f",
+        value_display_char_form_feed,
     );
     test_value!(
-        value_display_char_carriage_return,
         Value::<Dummy>::Char('\r'),
-        "?\\r"
+        "?\\r",
+        value_display_char_carriage_return,
     );
     test_value!(
-        value_display_char_escape,
         Value::<Dummy>::Char('\u{1b}'),
-        "?\\e"
+        "?\\e",
+        value_display_char_escape,
     );
-    test_value!(value_display_char_space, Value::<Dummy>::Char(' '), "?\\s");
+    test_value!(Value::<Dummy>::Char(' '), "?\\s", value_display_char_space);
     test_value!(
-        value_display_char_delete,
         Value::<Dummy>::Char('\u{7f}'),
-        "?\\d"
+        "?\\d",
+        value_display_char_delete,
     );
     test_value!(
-        value_display_char_backslash,
         Value::<Dummy>::Char('\\'),
-        "?\\\\"
+        "?\\\\",
+        value_display_char_backslash,
     );
     test_value!(
-        value_display_char_double_quote,
         Value::<Dummy>::Char('\"'),
-        "?\\\""
+        "?\\\"",
+        value_display_char_double_quote,
     );
     test_value!(
-        value_display_char_left_paren,
         Value::<Dummy>::Char('('),
-        "?\\("
+        "?\\(",
+        value_display_char_left_paren,
     );
     test_value!(
-        value_display_char_right_paren,
         Value::<Dummy>::Char(')'),
-        "?\\)"
+        "?\\)",
+        value_display_char_right_paren,
     );
     test_value!(
-        value_display_char_left_square_bracket,
         Value::<Dummy>::Char('['),
-        "?\\["
+        "?\\[",
+        value_display_char_left_square_bracket,
     );
     test_value!(
-        value_display_char_right_square_bracket,
         Value::<Dummy>::Char(']'),
-        "?\\]"
+        "?\\]",
+        value_display_char_right_square_bracket,
     );
     test_value!(
-        value_display_char_semicolon,
         Value::<Dummy>::Char(';'),
-        "?\\;"
+        "?\\;",
+        value_display_char_semicolon,
     );
-    test_value!(value_display_char_pipe, Value::<Dummy>::Char('|'), "?\\|");
+    test_value!(Value::<Dummy>::Char('|'), "?\\|", value_display_char_pipe);
     test_value!(
-        value_display_char_single_quote,
         Value::<Dummy>::Char('\''),
-        "?\\\'"
+        "?\\\'",
+        value_display_char_single_quote,
     );
     test_value!(
-        value_display_char_back_quote,
         Value::<Dummy>::Char('`'),
-        "?\\`"
+        "?\\`",
+        value_display_char_back_quote,
     );
-    test_value!(value_display_char_hash, Value::<Dummy>::Char('#'), "?\\#");
-    test_value!(value_display_char_period, Value::<Dummy>::Char('.'), "?\\.");
-    test_value!(value_display_char_comma, Value::<Dummy>::Char(','), "?\\,");
+    test_value!(Value::<Dummy>::Char('#'), "?\\#", value_display_char_hash);
+    test_value!(Value::<Dummy>::Char('.'), "?\\.", value_display_char_period);
+    test_value!(Value::<Dummy>::Char(','), "?\\,", value_display_char_comma);
 
-    test_value!(value_display_char_regular, Value::<Dummy>::Char('a'), "?a");
+    test_value!(Value::<Dummy>::Char('a'), "?a", value_display_char_regular);
 
     test_value!(
-        value_display_string_hello_world,
         Value::<Dummy>::String("hello world"),
-        "\"hello world\""
+        "\"hello world\"",
+        value_display_string_hello_world,
     );
     test_value!(
-        value_display_string_control_g,
         Value::<Dummy>::String("\u{7}"),
-        "\"\\a\""
+        "\"\\a\"",
+        value_display_string_control_g,
     );
     test_value!(
-        value_display_string_backspace,
         Value::<Dummy>::String("\u{8}"),
-        "\"\\b\""
+        "\"\\b\"",
+        value_display_string_backspace,
     );
     test_value!(
-        value_display_string_tab,
         Value::<Dummy>::String("\t"),
-        "\"\\t\""
+        "\"\\t\"",
+        value_display_string_tab,
     );
     test_value!(
-        value_display_string_vertical_tab,
         Value::<Dummy>::String("\u{b}"),
-        "\"\\v\""
+        "\"\\v\"",
+        value_display_string_vertical_tab,
     );
     test_value!(
-        value_display_string_form_feed,
         Value::<Dummy>::String("\u{c}"),
-        "\"\\f\""
+        "\"\\f\"",
+        value_display_string_form_feed,
     );
     test_value!(
-        value_display_string_carriage_return,
         Value::<Dummy>::String("\r"),
-        "\"\\r\""
+        "\"\\r\"",
+        value_display_string_carriage_return,
     );
     test_value!(
-        value_display_string_escape,
         Value::<Dummy>::String("\u{1b}"),
-        "\"\\e\""
+        "\"\\e\"",
+        value_display_string_escape,
     );
     test_value!(
-        value_display_string_backslash,
         Value::<Dummy>::String("\\"),
-        "\"\\\\\""
+        "\"\\\\\"",
+        value_display_string_backslash,
     );
     test_value!(
-        value_display_string_double_quote,
         Value::<Dummy>::String("\""),
-        "\"\\\"\""
+        "\"\\\"\"",
+        value_display_string_double_quote,
     );
     test_value!(
-        value_display_string_delete,
         Value::<Dummy>::String("\u{7f}"),
-        "\"\\d\""
+        "\"\\d\"",
+        value_display_string_delete,
+    );
+
+    test_value!(Value::<Dummy>::Symbol(""), "##", value_display_symbol_empty);
+    test_value!(
+        Value::<Dummy>::Symbol("hello-world"),
+        "hello-world",
+        value_display_symbol_hello_world
+    );
+
+    test_value!(
+        Value::<Dummy>::Symbol(" "),
+        "\\ ",
+        value_display_symbol_space
+    );
+    test_value!(
+        Value::<Dummy>::Symbol("("),
+        "\\(",
+        value_display_symbol_left_paren
+    );
+    test_value!(
+        Value::<Dummy>::Symbol(")"),
+        "\\)",
+        value_display_symbol_right_paren
+    );
+    test_value!(
+        Value::<Dummy>::Symbol("["),
+        "\\[",
+        value_display_symbol_left_square_bracket
+    );
+    test_value!(
+        Value::<Dummy>::Symbol("]"),
+        "\\]",
+        value_display_symbol_right_square_bracket
+    );
+    test_value!(
+        Value::<Dummy>::Symbol("\\"),
+        "\\\\",
+        value_display_symbol_backslash
+    );
+    test_value!(
+        Value::<Dummy>::Symbol(";"),
+        "\\;",
+        value_display_symbol_semicolon
+    );
+    test_value!(
+        Value::<Dummy>::Symbol("\""),
+        "\\\"",
+        value_display_symbol_double_quote
+    );
+    test_value!(
+        Value::<Dummy>::Symbol("|"),
+        "\\|",
+        value_display_symbol_pipe,
+    );
+    test_value!(
+        Value::<Dummy>::Symbol("\'"),
+        "\\\'",
+        value_display_symbol_quote
+    );
+    test_value!(
+        Value::<Dummy>::Symbol("`"),
+        "\\`",
+        value_display_symbol_backquote
+    );
+    test_value!(
+        Value::<Dummy>::Symbol("#"),
+        "\\#",
+        value_display_symbol_hash
+    );
+    test_value!(
+        Value::<Dummy>::Symbol("."),
+        "\\.",
+        value_display_symbol_period
+    );
+    test_value!(
+        Value::<Dummy>::Symbol(","),
+        "\\,",
+        value_display_symbol_comma
     );
 }
